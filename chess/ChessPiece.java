5f3fb5a6e3225d66c54e5e0f0879b3c9a6d35f0e
nt4m <nalume4@hotmail.com>
2020-05-27 11:31:11 -0400

    Piece possible movies

--- chess/ChessPiece.java
+++ chess/ChessPiece.java
@@ -3,7 +3,7 @@ package javachessgame.chess;
 import javachessgame.boardgame.Board;
 import javachessgame.boardgame.Piece;
 
-public class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece{
     private Color color;
     
     public ChessPiece(Board board, Color color){
