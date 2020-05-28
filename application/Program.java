d57d8ed3bae2c780beffbcaa8c32d4cbd81add85
nt4m <nalume4@hotmail.com>
2020-05-27 22:38:48 -0400

    Check logic

--- application/Program.java
+++ application/Program.java
@@ -27,7 +27,6 @@ public class Program {
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
