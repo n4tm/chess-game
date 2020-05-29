5fe5385627bfdd211491337701dd53b69f3af0ca
nt4m <nalume4@hotmail.com>
2020-05-29 00:28:31 -0400

    Fix - better treatment to promotion

--- chess/ChessException.java
+++ chess/ChessException.java
@@ -4,6 +4,7 @@ import javachessgame.boardgame.BoardException;
 
 public class ChessException extends BoardException {
     private static final long serialVersionUID = 1L;
+
     public ChessException(String msg) {
         super(msg);
     }
